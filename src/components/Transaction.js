e12a4566c5128fee4ea351b557fa752970243913
Ilia Lobsanov <ilia@nurey.com>
2021-06-21 08:53:49 -0400

    Highlight empty notes for debits

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,13 +3,18 @@ import UpdateTransaction from './UpdateTransaction'
 
 const Transaction = (props) => {
   const { transaction } = props;
+  var noteClasses = ['note'];
+  if ( transaction.debit && ( !transaction.note || transaction.note?.detail === '' ) ) {
+    noteClasses.push('empty');
+  }
+
   return (
     <div>
       <div>date: {transaction.txDate}</div>
       <div>credit: {transaction.credit}</div>
       <div>debit: {transaction.debit}</div>
       <div>details: {transaction.details}</div>
-      <div>note: {transaction.note?.detail}</div>
+      <div class={ noteClasses.join(' ') }>note: {transaction.note?.detail}</div>
       <UpdateTransaction id={transaction.id} noteId={transaction.note?.id} />
     </div>
   );
